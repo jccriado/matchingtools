429dd3bf792c75b34d8c204a5f5e518d2145b185
Juan Carlos Criado Alamo <jccc01@correo.ugr.es>
2017-04-10 14:26:56 +0200

    Added license and classifiers

--- setup.py
+++ setup.py
@@ -23,6 +23,17 @@ setup(
     author='Juan Carlos Criado Alamo',
     author_email='jccriadoalamo@ugr.es',
 
+    license='MIT',
+
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Physics'],
+
     keywords=['effective field theory symbolic tree level integration'],
 
     packages=['effective', 'effective.extras']
