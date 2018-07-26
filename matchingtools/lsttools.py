fa21bcac2c73256a9d677a7031fa704381ebf55c
Juan Carlos Criado Alamo <jccc01@correo.ugr.es>
2018-07-26 10:27:08 +0200

    Fixed lsttools.iterate

--- matchingtools/lsttools.py
+++ matchingtools/lsttools.py
@@ -11,6 +11,8 @@ def enum_product(*iters):
 
 def iterate(f, x, n):
     if n > 0:
-        return f(iterate(f, x, n - 1))
+        iterated_collection = iterate(f, x, n - 1)
+        iterated_collection.append(f(iterated_collection[-1]))
+        return iterated_collection
     else:
-        return x
+        return [x]
