0dea7c56f87241c7ad60c80b50ac182e47af0ffd
Juan Carlos Criado Alamo <jccc01@correo.ugr.es>
2018-07-26 10:00:27 +0200

    Added iterate function to lsttools

--- matchingtools/lsttools.py
+++ matchingtools/lsttools.py
@@ -7,3 +7,10 @@ def concat(lst):
 
 def enum_product(*iters):
     return itertools.product(*map(enumerate, iters))
+
+
+def iterate(f, x, n):
+    if n > 0:
+        return f(iterate(f, x, n - 1))
+    else:
+        return x
