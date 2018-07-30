6744c5b793e56389e4436d4a3b18b5effbeb9bb7
Juan Carlos Criado Alamo <jccc01@correo.ugr.es>
2018-07-31 00:12:10 +0200

    Add a simple permutation-matching test case with free indices

--- test/test_permutation_match.py
+++ test/test_permutation_match.py
@@ -8,13 +8,16 @@ i, j = Index.make('i', 'j')
 x, y = RealField.make('x', 'y', statistics=Statistics.BOSON, dimension=1)
 
 class TestPermutationMatch(unittest.TestCase):
-    def test_permutation_same_field(self):
+    def test_permutation_with_other_match(self):
         self.assertIsNotNone(
             Match.match_operators(
                 (x(i) * x(j) * y(i, j))._to_operator(),
                 (x(j) * x(i) * y(i, j))._to_operator()
             )
         )
+
+    def test_permutation_eq(self):
+        self.assertEqual(x(i) * x(j), x(j) * x(i))
 
 if __name__ == "__main__":
     unittest.main()
