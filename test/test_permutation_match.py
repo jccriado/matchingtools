5a9a15bfb66b2aaecae8d2ce80a276cca97fbbfe
Nacho Mas <nacho.cronos2@gmail.com>
2018-07-31 12:43:16 +0200

    Minor style changes to test case (now passing)

--- test/test_permutation_match.py
+++ test/test_permutation_match.py
@@ -4,17 +4,20 @@ from matchingtools.matches import Match
 from matchingtools.indices import Index
 from matchingtools.core import RealField, Statistics
 
+
 i, j = Index.make('i', 'j')
 x, y = RealField.make('x', 'y', statistics=Statistics.BOSON, dimension=1)
 
+
 class TestPermutationMatch(unittest.TestCase):
     def test_permutation_same_field(self):
+        pattern = (x(i) * x(j) * y(i, j))._to_operator()
+        target = (x(j) * x(i) * y(i, j))._to_operator()
+
         self.assertIsNotNone(
-            Match.match_operators(
-                (x(i) * x(j) * y(i, j))._to_operator(),
-                (x(j) * x(i) * y(i, j))._to_operator()
-            )
+            Match.match_operators(pattern, target)
         )
+
 
 if __name__ == "__main__":
     unittest.main()
