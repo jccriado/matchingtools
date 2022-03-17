79f48b94448fcdabd4341e55fd9fb5732dd09f55
Juan Carlos Criado <juan.c.criado@durham.ac.uk>
2022-03-17 17:07:42 +0000

    make all interface functions and classes available at the top namespace

--- setup.py
+++ setup.py
@@ -7,34 +7,27 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, "README.rst"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='matchingtools',
-
-    version='0.3.1',
-
-    description='A library for symbolic Effective Field Theory calculations',
+    name="matchingtools",
+    version="0.4.1",
+    description="A library for symbolic Effective Field Theory calculations",
     long_description=long_description,
-
-    url='https://github.com/jccriado/matchingtools',
-
-    author='Juan Carlos Criado Alamo',
-    author_email='jccriadoalamo@ugr.es',
-
-    license='MIT',
-
+    url="https://github.com/jccriado/matchingtools",
+    author="Juan Carlos Criado Alamo",
+    author_email="jccriadoalamo@ugr.es",
+    license="MIT",
     classifiers=[
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
-        'Topic :: Scientific/Engineering :: Physics'],
-
-    keywords=['effective field theory symbolic tree matching integration'],
-
-    packages=['matchingtools', 'matchingtools.extras']
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Physics",
+    ],
+    keywords=["effective field theory symbolic tree matching integration"],
+    packages=["matchingtools", "matchingtools.extras"],
 )
